5b18890267410e465f0874bd84b17513774e8bc4
David Morasz <m@mcro.de>
2016-05-21 00:55:00 +0200

    bpgstatus lite

--- assets/js/bpgloader.js
+++ assets/js/bpgloader.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var ctr = 0;
     var oil = function() {
-        document.getElementById("bpgstatus").innerHTML = "Loading BPG " + ctr;
+        console.log("Loading BPG " + ctr);
         ctr++;
     };
     bpgDecode(oil);
