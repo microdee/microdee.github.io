81cb499609dabc850ab584d0c2bfef2fb57fdb97
microdee <m@mcro.de>
2020-05-17 16:25:42 +0200

    app is now a component class

--- components/App.jsx
+++ components/App.jsx
@@ -1,35 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-function App({ children, routes }) {
-
-  function generateMapMenu() {
-    let path = '';
-    return (
-      routes.filter(route => route.mapMenuTitle)
-        .map((route, index, array) => (
-          <span key={index}>
-            <Link
-              to={path += ((path.slice(-1) === '/' ? '' : '/') +
-                  (route.path === '/' ? '' : route.path))}
-            >
-              {route.mapMenuTitle}
-            </Link>
-            {(index + 1) < array.length && ' / '}
-          </span>
-        ))
-    );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  return(
-    <div style={{maxWidth: '500px'}}>
-      <h2 style={{marginBottom: 0}}>Building here a new website</h2>
-      <nav>
-        {generateMapMenu()}
-      </nav>
-      {children}
-    </div>
-  );
-}
+  render() {
+    const { children, routes } = this.props;
 
-export default App;
+    return(
+      <div style={{maxWidth: '500px'}}>
+        <h2 style={{marginBottom: 0}}>Building here a new website</h2>
+        {children}
+      </div>
+    );
+  }
+}
