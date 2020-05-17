6b6759b2c71a71338f2b27391499f899c5fe42f1
microdee <m@mcro.de>
2020-05-17 16:49:41 +0200

    404 error message is infinitely better

--- components/PageNotFound.jsx
+++ components/PageNotFound.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 function PageNotFound({ location }) {
   return (
     <p>
-      Page not found - the path, {location.pathname}, did not match any
-      React Router routes.
+      I'm sorry but, <b>{location.pathname}</b>, is not a thing.
     </p>
   );
 }
