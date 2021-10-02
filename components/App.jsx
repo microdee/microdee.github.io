903d7956e0592b00c24138c8d3718d39aee1985d
David Morasz <m@mcro.de>
2021-10-02 02:54:08 +0200

    trying out upcoming Animation Worklet api

--- components/App.jsx
+++ components/App.jsx
@@ -8,6 +8,45 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
     }
+
+    componentDidMount()
+    {
+        function registerScrollTest()
+        {
+            let root = document.getElementById("root");
+            let scrolltest = document.getElementById("scrolltest");
+            var scrollRange = root.scrollHeight - root.clientHeight;
+            return new WorkletAnimation('scroll-test',
+                new KeyframeEffect(scrolltest, [
+                    {transform: 'translateY(0)'},
+                    {transform: `translateY(${scrollRange}px)`}
+                ], scrollRange),
+                new ScrollTimeline({
+                    scrollSource: root,
+                    orientation: 'block',
+                    timeRange: scrollRange
+                })
+            );
+        }
+        
+        window.testAnimator = registerScrollTest();
+        window.testAnimator.play();
+
+        new ResizeObserver(e => {
+            const [{
+                borderBoxSize,
+                contentBoxSize,
+                contentRect,
+                devicePixelContentBoxSize: [devicePixelBoxSize]
+            }] = e;
+            console.log(borderBoxSize, contentBoxSize, contentRect, devicePixelBoxSize)
+
+            window.testAnimator?.cancel();
+            window.testAnimator = registerScrollTest();
+            window.testAnimator.play();
+            
+        }).observe(document.querySelector("#appRoot"));
+    }
     
     render() {
         const { children, location } = this.props;
@@ -16,6 +55,18 @@ export default class App extends React.Component {
         {
             return(
                 <div id="appRoot">
+                    <div
+                        id="scrolltest"
+                        style={{
+                            position: "absolute",
+                            top: 60,
+                            left: 100,
+                            width: 100,
+                            height: 100,
+                            background: "#FFF",
+                            zIndex: 20000
+                        }}
+                    ></div>
                     <Logo />
                     <Gh1 className="h0" glitchtype="1" id="mcrode"><Link to="/">mcro.de</Link></Gh1>
                     <MainMenu />
