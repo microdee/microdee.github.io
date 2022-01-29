85347bacb16aee85248e9823b1eb0f966196ff7f
David Morasz <m@mcro.de>
2022-01-29 19:33:52 +0100

    ^ improved intro video

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,26 +1,53 @@
 import React from 'react';
+import ParallaxEffect from './parallax';
 
-export default function Logo({ useShaderToy }) {
-    return useShaderToy ? (
-        <div className="logo shadertoy">
-            <iframe
-                className="logo shadertoy"
-                width="100%"
-                height="107%"
-                frameborder="0"
-                src="https://www.shadertoy.com/embed/WlXcDH?gui=false&t=10&paused=false&muted=true"
-                allowfullscreen
-            ></iframe>
-        </div>
-    ) : (
-        <div className="logo">
-            <div className="pleaseScroll">
-                <div className="indicator">
+export default class Logo extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            parallax: new ParallaxEffect()
+        };
+        this.mainDiv = React.createRef();
+    }
+
+    componentDidMount() {
+        this.state.parallax.register(this.mainDiv.current);
+    }
+
+    render() {
+        let divcontainerStyle = {
+            height: "100%"
+        };
+        return this.props.useShaderToy ? (
+            <div className="logo shadertoy">
+                <div className="pleaseScroll">
+                    <div className="indicator">
+                    </div>
+                </div>
+                <div ref={this.mainDiv} style={divcontainerStyle}>
+                    <iframe
+                        className="logo shadertoy"
+                        width="100%"
+                        height="107%"
+                        frameborder="0"
+                        src="https://www.shadertoy.com/embed/WlXcDH?gui=false&t=10&paused=false&muted=true"
+                        allowfullscreen
+                    ></iframe>
                 </div>
             </div>
-            <video loop muted autoPlay>
-                <source src="/mcrode.logo.webm" type="video/webm" />
-            </video>
-        </div>
-    );
+        ) : (
+            <div className="logo">
+                <div className="pleaseScroll">
+                    <div className="indicator">
+                    </div>
+                </div>
+                <div ref={this.mainDiv} style={divcontainerStyle}>
+                    <video loop muted autoPlay>
+                        <source src="/mcrode.logo.webm" type="video/webm" />
+                    </video>
+                </div>
+            </div>
+        );
+    }
 }
