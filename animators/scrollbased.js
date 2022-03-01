f8019d4a03a06160d9a86ff8c5442782f395ead2
David Morasz <m@mcro.de>
2022-03-01 03:02:52 +0100

    - webanim

--- animators/scrollbased.js
+++
@@ -1,7 +0,0 @@
-registerAnimator('scrollbased', class ScrollBasedAnimator {
-    animate(currentTime, effect) {
-      if (currentTime == NaN) return;
-      effect.localTime = 1.0 * currentTime;
-    }
-  }
-);
